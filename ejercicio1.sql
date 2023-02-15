3e1e4a9d2c02b2d0924412e9a388345b8520942f
nerea <nlopsan0104@g.educaand.es>
2023-02-15 12:54:27 +0100

    Añade ejercicio1 del tema 5

--- ejercicio1.sql
+++ ejercicio1.sql
@@ -1,77 +1,80 @@
 /*
-ESQUEMA RELACIONAL
--> empleados(pk[numempleado], extelefon, fecnacim, fecingreso, 
-salario, comision, numhijos, nomemp);
--> deptos(pk[numdepto, numcentro*], presupuesto, nomdepto, numdepto*);
--> centros(pk[numcentro], nomcentro, direccion);
--> dir(pk[numempleado, [numdepto, numcentro]*], feninidir, fecfindir);
+ESQUEMA RELACIONAL:
+centros(pk[numce], nomce, dirce)
+deptos(pk[numce*, numde],.... [deptodepen, centrodepen]*)
+empleados(pk[numem], ...., [numce, numde]*)
+dirigir(pk[numem*, [numce,numde]*, fecinidir], fecfindir)
 */
 
 drop database if exists ejercicio1;
 create database if not exists ejercicio1;
 use ejercicio1;
 
--- -> empleados(pk[numempleado], extelefon, fecnacim, fecingreso, 
--- salario, comision, numhijos, nomemp);
-drop table if exists empleados;
-create table if not exists empleados
-(
-	numempleado int unsigned,
-    extelefon char(12),
-    fecnacim date,
-    fecingreso date,
-    salario double(10, 2),
-    comision double(10, 2),
-    numhijos int unsigned,
-    nomemp varchar(60),
-    constraint pk_empleados primary key (numempleado)
-);
-
--- centros(pk[numcentro], nomcentro, direccion);
+/* CREAMOS LAS TABLAS ==> EL ORDEN ES IMPORTANTE (INTEGRIDAD REFERENCIAL) */
 drop table if exists centros;
 create table if not exists centros
-(
-	numcentro int unsigned,
-    nomcentro varchar(60),
-    direccion varchar(60),
-    constraint pk_centros primary key (numcentro)
-);
-
--- deptos(pk[numdepto, numcentro*], presupuesto, nomdepto, numdepto*);
+    (numce int,
+     nomce varchar(60) not null,
+     dirce varchar(120),
+    constraint pk_centros primary key (numce)
+    );
+    
 drop table if exists deptos;
 create table if not exists deptos
-(
-	numdepto int unsigned,
-    numcentro int unsigned,
-    nomdepto varchar(60),
-    presupuesto decimal(10, 2),
-    deptodepen int unsigned,
-    constraint pk_deptos primary key (numdepto),
-    constraint fk_deptos_deptos foreign key (deptodepen)
-		references deptos (numdepto)
-        on delete no action on update no action,
-	constraint fk_deptos_centros foreign key (numcentro)
-		references centros (numcentro)
-        on delete no action on update cascade
-);
+    (numde int,
+     numce int,
+     nomde varchar(60) not null,
+     presude decimal (10,2),
+    constraint pk_deptos primary key (numde, numce),
+    constraint fk_deptos_centros foreign key (numce)
+        references centros(numce) 
+            on delete no action on update cascade
+    );
 
--- dir(pk[numempleado, [numdepto, numcentro]*], fecinidir, fecfindir);
-drop table if exists dir;
-create table if not exists dir
-(
-	numdepto int unsigned,
-    numcentro int unsigned,
-    numempleado int unsigned,
-    fecinidir date,
-    fecfindir date null,
-    constraint pk_dir primary key (numdepto, numcentro, numempleado, fecinidir),
-    constraint fk_dir_deptos1 foreign key (numdepto)
-		references deptos (numdepto)
-        on delete no action on update cascade,
-	constraint fk_dir_deptos2 foreign key (numcentro)
-		references deptos (numcentro)
-        on delete no action on update cascade,
-	constraint fk_dir_empleados foreign key (numempleado)
-		references empleados (numempleado)
-        on delete no action on update cascade
-);
+drop table if exists empleados;
+create table if not exists empleados
+    (numem int,
+     numde int,
+     numce int,
+     extelem char(3) null,
+     fecnaem date null,
+     fecinem date not null,
+     salarem decimal (7,2),
+     comisem decimal (4,2),
+     numhiem tinyint unsigned,
+     nomem varchar(20) not null,
+     ape1em varchar(20) not null,
+     ape2em varchar(20) null,
+    constraint pk_empleados primary key (numem),
+    constraint fk_empleados_deptos foreign key (numde, numce)
+        references deptos (numde, numce)
+            on delete no action on update cascade
+    );
+    
+drop table if exists dirigir;
+create table if not exists dirigir
+    (numemdir int, -- wejfskjfsjkdf
+     numde int,
+     numce int,
+     fecinidir date,
+     fecfindir date null,
+    constraint pk_dirigir primary key (numemdir, numde, numce, fecinidir),
+    constraint fk_dirigir_empleados foreign key (numemdir)
+        references empleados (numem) on delete no action on update cascade,
+    constraint fk_dirigir_deptos foreign key (numde, numce)
+        references deptos (numde, numce) on delete no action on update cascade
+    );
+
+/*  DESPUES DE EJECUTAR DESCUBRIMOS QUE NOS FALTA REPRESENTAR
+    LA RELACIÓN DEP (deptos a deptos) */
+
+ALTER TABLE deptos
+	add column deptodepen int,
+	add column centrodepen int,
+	add constraint fk_deptos_deptos foreign key (deptodepen, centrodepen)
+		references deptos (numde, numce)
+			on delete no action on update cascade;
+	
+
+
+
